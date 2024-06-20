363a100e0c71950aeac2862668c3fc29817ff2c0
fallbackerik <fallbackerik@gmail.com>
2024-06-20 14:51:34 +0200

    Update generate_includes.py

--- generate_includes.py
+++ generate_includes.py
@@ -2,12 +2,16 @@ import os
 import os.path
 
 def main() -> None:
-  with open("index.adoc", "w") as index:
-    for journal in os.listdir("journal/"):
-      full_path = os.path.join("journal/", journal)
-      print("including: ", full_path)
-      if os.path.isfile(full_path) and journal.endswith(".adoc"):
-        index.append("include::{journal}[]")
-      
+  journal_includes = []
+  for journal in os.listdir("journal/"):
+    full_path = os.path.join("journal/", journal)
+    print("including: ", full_path)
+    if os.path.isfile(full_path) and journal.endswith(".adoc"):
+      journal_includes.append("include::{full_path}[]")
+  with open("index.adoc", "r") as index_in:
+    index_txt = index_in.read()
+  with open("index.adoc", "w") as index_out:
+    index_out.write(index_txt.replace("include::journal/*[]", "\n".join(journal_includes)))
+
 if __name__ == '__main__':
   main()
