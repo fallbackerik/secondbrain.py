b2dac3d95005bf98581dfd6caf5a1b8a144296f4
Erik <fallbackerik@gmail.com>
2024-06-24 15:39:15 +0200

    Update call_asciidoc.py

--- call_asciidoc.py
+++ call_asciidoc.py
@@ -3,7 +3,85 @@ import os
 import os.path
 
 from asciidoc import asciidoc
+from asciidoc.asciidoc import asciidoc, reset_asciidoc
 import argparse
+
+def execute(cmd, opts, args):
+    """
+    """
+    reset_asciidoc()
+    config.init()
+    if len(args) > 1:
+        usage('Too many arguments')
+        sys.exit(1)
+    backend = None
+    doctype = None
+    confiles = []
+    outfile = None
+    options = []
+    help_option = False
+    for o, v in opts:
+        if o in ('-b', '--backend'):
+            backend = v
+        if o in ('-c', '--dump-conf'):
+            options.append('-c')
+        if o in ('-d', '--doctype'):
+            doctype = v
+        if o in ('-e', '--no-conf'):
+            options.append('-e')
+        if o in ('-f', '--conf-file'):
+            confiles.append(v)
+        if o == '--filter':
+            config.filters.append(v)
+        if o in ('-n', '--section-numbers'):
+            o = '-a'
+            v = 'numbered'
+        if o == '--theme':
+            o = '-a'
+            v = 'theme=' + v
+        if o in ('-a', '--attribute'):
+            e = parse_entry(v, allow_name_only=True)
+            if not e:
+                usage('Illegal -a option: %s' % v)
+                sys.exit(1)
+            k, v = e
+            # A @ suffix denotes don't override existing document attributes.
+            if v and v[-1] == '@':
+                document.attributes[k] = v[:-1]
+            else:
+                config.cmd_attrs[k] = v
+        if o in ('-o', '--out-file'):
+            outfile = v
+        if o in ('-s', '--no-header-footer'):
+            options.append('-s')
+        if o in ('-v', '--verbose'):
+            options.append('-v')
+    stdin, stdout = sys.stdin, sys.stdout
+    try:
+        infile = args[0]
+        if infile == '-':
+            infile = '<stdin>'
+        elif isinstance(infile, str):
+            infile = os.path.abspath(infile)
+        else:   # Input file is file object from API call.
+            sys.stdin = infile
+            infile = '<stdin>'
+        if outfile == '-':
+           outfile = '<stdout>'
+        elif isinstance(outfile, str):
+           outfile = os.path.abspath(outfile)
+        elif outfile is None:
+            if infile == '<stdin>':
+                outfile = '<stdout>'
+        else:   # Output file is file object from API call.
+            sys.stdout = outfile
+            outfile = '<stdout>'
+        # Do the work.
+        asciidoc(backend, doctype, confiles, infile, outfile, options)
+        if document.has_errors:
+            sys.exit(1)
+    finally:
+      sys.stdin, sys.stdout = stdin, stdout
 
 def main() -> None:
   """ use more core asciidoc functionality
@@ -22,6 +100,9 @@ def main() -> None:
   print("call asciidoc", args)
   sys.stdout.flush()
 
+
+
+    
   for file in args.infiles:
     filepath, filename = os.path.split(file)
     fname_wo_ending = filename.split(".")[0]
