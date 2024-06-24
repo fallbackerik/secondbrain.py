606e96f197ac43e931c80eee294ce728b4aaabd2
Erik <fallbackerik@gmail.com>
2024-06-24 14:31:14 +0200

    Update call_asciidoc.py

--- call_asciidoc.py
+++ call_asciidoc.py
@@ -4,7 +4,17 @@ from asciidoc import asciidoc
 def main() -> None:
   """ use more core asciidoc functionality
   """
-  print("call asciidoc")
+  
+
+  parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+  parser.add_argument('-i', '--infiles')
+
+  #args = parser.parse_args()
+  print("call asciidoc", argv)
   asciidoc.cli(sys.argv)
   # from the asciidoc codebase, these are the args asciidoc is looking for:
   # argv=['attribute=', 'backend=', 'conf-file=', 'doctype=', 'dump-conf',
