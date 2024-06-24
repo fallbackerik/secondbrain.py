1defe19d658cd03e32b8dd559012c9c579f2cd2d
Erik <fallbackerik@gmail.com>
2024-06-24 14:24:55 +0200

    Create call_asciidoc.py

---
+++ call_asciidoc.py
@@ -0,0 +1,16 @@
+import sys
+from asciidoc import asciidoc
+
+def main() -> None:
+  """ use more core asciidoc functionality
+  """
+  print("call asciidoc")
+  asciidoc.cls(sys.argv)
+  # from the asciidoc codebase, these are the args asciidoc is looking for:
+  # argv=['attribute=', 'backend=', 'conf-file=', 'doctype=', 'dump-conf',
+  # 'help', 'no-conf', 'no-header-footer', 'out-file=',
+  # 'section-numbers', 'verbose', 'version', 'safe', 'unsafe',
+  # 'doctest', 'filter=', 'theme=']
+
+if __name__ == '__main__':
+  main()
