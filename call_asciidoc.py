b33ab9e6cd918480e4ba0b2df96a2d1d48da4906
Erik <fallbackerik@gmail.com>
2024-06-24 14:26:25 +0200

    Update call_asciidoc.py

--- call_asciidoc.py
+++ call_asciidoc.py
@@ -5,7 +5,7 @@ def main() -> None:
   """ use more core asciidoc functionality
   """
   print("call asciidoc")
-  asciidoc.cls(sys.argv)
+  asciidoc.cli(sys.argv)
   # from the asciidoc codebase, these are the args asciidoc is looking for:
   # argv=['attribute=', 'backend=', 'conf-file=', 'doctype=', 'dump-conf',
   # 'help', 'no-conf', 'no-header-footer', 'out-file=',
