7c8a5fdfb822d42d841b8bdfba993a593ece76d2
Erik <fallbackerik@gmail.com>
2024-06-24 15:44:13 +0200

    Update call_asciidoc.py

--- call_asciidoc.py
+++ call_asciidoc.py
@@ -4,7 +4,7 @@ import os.path
 
 from asciidoc import asciidoc
 from asciidoc.asciidoc import asciidoc as fasciidoc
-from asciidoc.asciidoc import reset_asciidoc
+from asciidoc.asciidoc import reset_asciidoc, config
 import argparse
 
 def execute(cmd, opts, args):
