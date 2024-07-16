857d062149df03a0cd00b85e3bfa7364981cd9e2
Erik <fallbackerik@gmail.com>
2024-07-16 14:40:33 +0200

    Create add_note.py

---
+++ tools/add_note.py
@@ -0,0 +1,23 @@
+import jinja2
+import datetime
+
+today = datetime.date.today()
+title = "Test Note Creation"
+fname = "".join(title.split()) + ".adoc"
+context = {
+  "title": title,
+  "fname": fname,
+  "today_object" : today,
+  "today_iso_underscored" : today.strftime("%Y_%m_%d"),
+  "today_human_readable" : today.strftime("%A, %B %d, %Y"),
+}
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader("template/"))
+template = env.get_template("note.adoc.jinja")
+
+rendered = template.render(context)
+output_name = f"note/{title}.adoc"
+with open(output_name, "w", encoding="utf-8") as output:
+  output.write(rendered)
+
+print(f"created note {output_name}.")
