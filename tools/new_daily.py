d03fd86973f20707cebac9e9f299fd25441bb52b
Erik <fallbackerik@gmail.com>
2024-07-16 14:58:01 +0200

    Make new_daily.py importable

--- tools/new_daily.py
+++ tools/new_daily.py
@@ -1,19 +1,23 @@
 import jinja2
 import datetime
 
-today = datetime.date.today()
-context = {
-  "today_object" : today,
-  "today_iso_underscored" : today.strftime("%Y_%m_%d"),
-  "today_human_readable" : today.strftime("%A, %B %d, %Y"),
-}
+def main():
+  today = datetime.date.today()
+  context = {
+    "today_object" : today,
+    "today_iso_underscored" : today.strftime("%Y_%m_%d"),
+    "today_human_readable" : today.strftime("%A, %B %d, %Y"),
+  }
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader("template/"))
-template = env.get_template("journal.adoc.jinja")
+  env = jinja2.Environment(loader=jinja2.FileSystemLoader("template/"))
+  template = env.get_template("journal.adoc.jinja")
 
-rendered = template.render(context)
-output_name = f"journal/{context['today_iso_underscored']}.adoc"
-with open(output_name, "w", encoding="utf-8") as output:
-  output.write(rendered)
+  rendered = template.render(context)
+  output_name = f"journal/{context['today_iso_underscored']}.adoc"
+  with open(output_name, "w", encoding="utf-8") as output:
+    output.write(rendered)
 
-print(f"created journal entry {output_name}.")
+  print(f"created journal entry {output_name}.")
+
+if __name__ == '__main__':
+  main()
